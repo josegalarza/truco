a3409d32e3ce3f04930af8f6beb637da6e29a031
Jose Galarza <jose@canva.com>
2021-03-14 14:41:41 +1100

    Fix test_mazo.test_mezclar (#9)

--- src/test_mazo.py
+++ src/test_mazo.py
@@ -7,15 +7,25 @@ def test_init():
     assert len(m.cartas) == 40
     assert m.cartas[0].get_id() == "1e"
 
+
 def test_mezclar():
     m = Mazo()
+    before = m.cartas[:]
     m.mezclar()
-    assert m.cartas[0].get_id() != "1e"
+    after = m.cartas
+    for c1, c2 in zip(before, after):
+        if c1.get_id() != c2.get_id():
+            assert True  # After mezclar, at least 1 carta is on a different position.
+            return
+    assert False
+
 
 def test_pop():
     m = Mazo()
+    before = len(m.cartas)
     m.pop_carta()
-    assert len(m.cartas) == 39
+    assert len(m.cartas) == before - 1
+
 
 def test_repartir():
     m = Mazo()
