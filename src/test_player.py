62124e53e6dc80a0d838b7a3a708d84e65d7dcef
Jose Galarza <jose@canva.com>
2021-05-09 19:56:24 +1000

    Player class (#14)

---
+++ src/test_player.py
@@ -0,0 +1,13 @@
+from .player import Player
+
+
+def test_init():
+    player = Player(name="Jose")
+    assert player is not None
+    assert player.name == "Jose"
+    assert player.cards == []
+
+
+def test_print():
+    player = Player(name="Jose")
+    assert str(player) == "Jose"
